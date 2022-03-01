0cbab9d8c2abe1bcbd015e8a335f5fe234225c9f
bcout <bcouturi@unb.ca>
2022-02-28 21:59:03 -0400

    Set up level data

--- Assets/Scripts/Data/LevelData.cs
+++ Assets/Scripts/Data/LevelData.cs
@@ -1,31 +1,49 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 
-public class LevelData : MonoBehaviour
-{
-    /**
-     * 
+public class LevelData : MonoBehaviour { 
+    /*
+     * This is a list of all the blocks that make up the level, in order
+     * blocks[0] is the first block, blocks[n-1] is the last
      */
     private List<Transform> blocks;
-    // Start is called before the first frame update
-    void Start()
+    private GameObject[] available_blocks;
+
+    private void Start()
     {
-        blocks = new List<Transform>();
+        available_blocks = Resources.LoadAll("Level Blocks") as GameObject[];
     }
 
-    public List<Transform> GetBlocks()
+    /*
+     * This function generates a sequence of blocks to form a level, drawing from available_blocks to do so.
+     * None of the blocks intersect, and each level is noticeably different from the one before it
+     * 
+     * The player uses this list to move through the level
+     */
+    public List<Transform> GenerateLevel()
+    {
+        // TODO: Implement this function
+        return blocks;
+    }
+
+    /*
+     * Returns the blocks that make up the level this script is attached to.
+     * This is the same list that was generated at the beginning, when GenerateLevel() was called
+     */
+    public List<Transform> GetLevelBlocks()
     {
         return blocks;
     }
 
     public void RemoveBlock(int index)
     {
+        //Opportunity to put some checks in here before the block is removed
         blocks.RemoveAt(index);
     }
 
     public void AddBlock(Transform block)
     {
+        //Opportunity to put some checks in here before the block is added
         blocks.Add(block);
     }
 }
