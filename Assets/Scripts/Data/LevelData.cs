94cfeac78164bc0822306ca8ca7f1a12604d0d83
bcout <bcouturi@unb.ca>
2022-02-28 21:45:58 -0400

    Removed curves from straights, started organizing data flow

---
+++ Assets/Scripts/Data/LevelData.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelData : MonoBehaviour
+{
+    /**
+     * 
+     */
+    private List<Transform> blocks;
+    // Start is called before the first frame update
+    void Start()
+    {
+        blocks = new List<Transform>();
+    }
+
+    public List<Transform> GetBlocks()
+    {
+        return blocks;
+    }
+
+    public void RemoveBlock(int index)
+    {
+        blocks.RemoveAt(index);
+    }
+
+    public void AddBlock(Transform block)
+    {
+        blocks.Add(block);
+    }
+}
