2ba5b75e0325282ac17cfb36c2f2a012cfa87835
bcout <bcouturi@unb.ca>
2022-02-28 23:00:43 -0400

    Started writing level generation precomputation program

---
+++ Assets/Scripts/PrecomputeLevels.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrecomputeLevels : MonoBehaviour
+{
+    private GameObject[] available_level_blocks;
+    // Start is called before the first frame update
+    void Start()
+    {
+        available_level_blocks = GetComponent<GameData>().available_level_blocks;
+        
+        /*
+         * Start with a block, spawn a random block after it. If their box colliders collide, remove that block
+         * and spawn a different one. Repeat until there are 8 blocks end to end.
+         */
+    }
+}
