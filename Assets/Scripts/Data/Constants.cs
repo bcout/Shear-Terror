85b5913b665f3bc99460cca870fae872cd7286cd
bcout <bcouturi@unb.ca>
2022-03-05 20:11:13 -0400

    Put level limit back to a normal value

--- Assets/Scripts/Data/Constants.cs
+++ Assets/Scripts/Data/Constants.cs
@@ -3,7 +3,7 @@
 public static class Constants
 {
     public const int NUM_PATHS = 3;
-    public const int NUM_BLOCKS_IN_LEVEL = 100;
+    public const int NUM_BLOCKS_IN_LEVEL = 10;
 
     // A block can be at most 200x200 units in area
     public const float MAX_BLOCK_SIZE = 200;
