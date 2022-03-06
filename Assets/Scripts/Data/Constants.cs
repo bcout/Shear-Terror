dc5b0438227cf66d2f95f015b0e8a1ab5deb7e92
bcout <bcouturi@unb.ca>
2022-03-05 20:07:35 -0400

    Added some polish to the level generation :)

--- Assets/Scripts/Data/Constants.cs
+++ Assets/Scripts/Data/Constants.cs
@@ -3,7 +3,7 @@
 public static class Constants
 {
     public const int NUM_PATHS = 3;
-    public const int NUM_BLOCKS_IN_LEVEL = 1000;
+    public const int NUM_BLOCKS_IN_LEVEL = 100;
 
     // A block can be at most 200x200 units in area
     public const float MAX_BLOCK_SIZE = 200;
