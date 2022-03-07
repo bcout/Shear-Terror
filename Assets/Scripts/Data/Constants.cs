070efc0c2eddb6cfcc40173518aa0ac8db88d450
bcout <bcouturi@unb.ca>
2022-03-07 19:38:44 -0400

    Added scene to put good levels in

--- Assets/Scripts/Data/Constants.cs
+++ Assets/Scripts/Data/Constants.cs
@@ -3,7 +3,7 @@
 public static class Constants
 {
     public const int NUM_LANES = 3;
-    public const int NUM_BLOCKS_IN_LEVEL = 10;
+    public const int NUM_BLOCKS_IN_LEVEL = 45;
 
     // A block can be at most 200x200 units in area
     public const float MAX_BLOCK_SIZE = 200;
