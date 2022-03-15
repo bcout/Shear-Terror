42d1250c433404966a7daa57ed2bc1522331cb5f
cole8888 <colelightfoot88@gmail.com>
2022-03-15 20:00:33 -0300

    rotation

---
+++ Assets/Scripts/titleCameraController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class titleCameraController : MonoBehaviour
+{
+    float xRotCounter = 0.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        xRotCounter += 2.0f * Time.deltaTime;
+        transform.localEulerAngles = new Vector3(0, xRotCounter, 0);
+    }
+}
