9853b57d5d0f9df4002dc3e6166710d6aea53ce3
bcout <bcouturi@unb.ca>
2022-03-15 14:57:24 -0300

    Levels spawn basic building blocks, and player moves along the paths

--- Assets/Scripts/Data/Constants.cs
+++ Assets/Scripts/Data/Constants.cs
@@ -8,7 +8,7 @@ public static class Constants
     // A block can be at most 200x200 units in area
     public const float MAX_BLOCK_SIZE = 200;
 
-    public const float BASE_MOVEMENT_SPEED = 0.5f;
+    public const float BASE_MOVEMENT_SPEED = 0.3f;
 
     public const int SHORT_LEVEL_ID = 1;
     public const int LONG_LEVEL_ID = 2;
