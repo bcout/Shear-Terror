fa2da57dc6792cf860d5b50ff1ebfd906188ecbb
Brennan Couturier <55854294+bcout@users.noreply.github.com>
2022-03-28 13:40:15 -0300

    Revert "Add options screen"

--- Assets/Scripts/Data/GameData.cs
+++ Assets/Scripts/Data/GameData.cs
@@ -4,26 +4,13 @@ public static class GameData
     public static KeyCode jump_key { get; set; }
     public static KeyCode move_left_key { get; set; }
     public static KeyCode move_right_key { get; set; }
-    public static KeyCode look_back_key { get; set; }
-    public static KeyCode flip_front_key { get; set; }
-    public static KeyCode flip_back_key { get; set; }
-    public static KeyCode roll_left_key { get; set; }
-    public static KeyCode roll_right_key { get; set; }
-    public static KeyCode spin_left_key { get; set; }
-    public static KeyCode spin_right_key { get; set; }
+    public static KeyCode spin_key { get; set; }
+
     static GameData()
     {
-        // jump_key = KeyCode.UpArrow;
         jump_key = KeyCode.UpArrow;
         move_left_key = KeyCode.LeftArrow;
         move_right_key = KeyCode.RightArrow;
-        look_back_key = KeyCode.DownArrow;
-
-        flip_front_key = KeyCode.W;
-        flip_back_key = KeyCode.S;
-        roll_left_key = KeyCode.A;
-        roll_right_key = KeyCode.D;
-        spin_left_key = KeyCode.Q;
-        spin_right_key = KeyCode.E;
+        spin_key = KeyCode.DownArrow;
     }
 }
