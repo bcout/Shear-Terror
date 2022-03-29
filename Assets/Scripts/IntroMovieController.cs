5cca3631dadfd09fcc52f6b09ca2baa34d3046c7
bcout <bcouturi@unb.ca>
2022-03-29 16:38:56 -0300

    Got intro scene back to where it was before

---
+++ Assets/Scripts/IntroMovieController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IntroMovieController : MonoBehaviour
+{
+    [SerializeField] Camera[] cameras;
+    [SerializeField] GameObject[] sheeps;
+    [SerializeField] GameObject[] farmers;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
