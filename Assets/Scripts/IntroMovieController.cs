915ceb4f3a1f89b5e2b7a05ef7d487d1a078479a
bcout <bcouturi@unb.ca>
2022-03-30 23:21:41 -0300

    Intro movie scenes complete

--- Assets/Scripts/IntroMovieController.cs
+++ Assets/Scripts/IntroMovieController.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class IntroMovieController : MonoBehaviour
 {
+    private const int NUM_SCENES = 8;
+
     [SerializeField] private GameObject[] scene_0_objects;
     [SerializeField] private GameObject[] scene_1_objects;
     [SerializeField] private GameObject[] scene_2_objects;
@@ -13,15 +15,64 @@ public class IntroMovieController : MonoBehaviour
     [SerializeField] private GameObject[] scene_6_objects;
     [SerializeField] private GameObject[] scene_7_objects;
 
+    private GameObject[][] scenes;
+
+    private int current_scene;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        InitScenes();
+        current_scene = 0;
+        LoadScene(current_scene);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            UnloadScene(current_scene);
+            current_scene++;
+            LoadScene(current_scene);
+        }
+    }
+
+    private void InitScenes()
+    {
+        scenes = new GameObject[][] { scene_0_objects, scene_1_objects, scene_2_objects, scene_3_objects, scene_4_objects, scene_5_objects, scene_6_objects, scene_7_objects };
+        for (int i = 0; i < scenes.Length; i++)
+        {
+            for (int j = 0; j < scenes[i].Length; j++)
+            {
+                scenes[i][j].SetActive(false);
+            }
+        }
+    }
+
+    private void UnloadScene(int scene_num)
+    {
+        if (scene_num >= NUM_SCENES || scene_num < 0)
+        {
+            return;
+        }
+
+        for (int i = 0; i < scenes[scene_num].Length; i++)
+        {
+            scenes[scene_num][i].SetActive(false);
+        }
+    }
+
+    private void LoadScene(int scene_num)
+    {
+        if (scene_num >= NUM_SCENES || scene_num < 0)
+        {
+            return;
+        }
+
+        for (int i = 0; i < scenes[scene_num].Length; i++)
+        {
+            scenes[scene_num][i].SetActive(true);
+        }
     }
 }
