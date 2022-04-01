dae4b16c3ccbb3fb7bea739e793c4d03c4854ecd
bcout <bcouturi@unb.ca>
2022-03-31 22:36:03 -0300

    removed leftover folders

--- Assets/Scripts/CreditsController.cs
+++ Assets/Scripts/CreditsController.cs
@@ -8,6 +8,11 @@ public class CreditsController : MonoBehaviour
     [SerializeField] private float speed = 25f;
     private const float MAX_Y_VALUE = 1372; // 17 * number of lines
 
+    private void Start()
+    {
+        transform.localPosition = new Vector3(transform.localPosition.x, 0f, transform.localPosition.z);
+    }
+
     private void Update()
     {
         if (transform.localPosition.y < MAX_Y_VALUE)
