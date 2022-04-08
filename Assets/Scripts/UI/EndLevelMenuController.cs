22000c1b740aa2b60283bc16b14e317637289a0e
bcout <bcouturi@unb.ca>
2022-04-08 13:58:33 -0300

    End level screen, continue to loading screen and exit to main menu

---
+++ Assets/Scripts/UI/EndLevelMenuController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndLevelMenuController : MonoBehaviour
+{
+
+    public void GoToLevelLoadingScreen()
+    {
+        print("Going to loading screen...");
+    }
+
+    public void ReturnToMainMenu()
+    {
+        SceneManager.LoadScene(Constants.MAIN_MENU_SCENE_NAME);
+    }
+}
