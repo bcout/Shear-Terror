ab8266366bdbf5c0e059a2a2a8278706f760f7ea
cole8888 <colelightfoot88@gmail.com>
2022-04-08 20:22:43 -0300

    weird bug on restart

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -9,30 +9,22 @@ using UnityEngine.UI;
 public class GameController : MonoBehaviour
 {
     [SerializeField] private GameObject sheep, farmer;
+    public LevelScreenController screen;
 
-    private GameObject heart1;
-    private GameObject heart2;
-    private GameObject heart3;
-    private GameObject heartsCanvas;
-    private GameObject gameOverCanvas;
-        
+    // private GameObject heartsCanvas;
+    // private GameObject gameOverCanvas;
     private LevelSpawner level_spawner;
     private SheepController sheep_controller;
     private FarmerController farmer_controller;
-    private AsyncOperation op;
 
     private bool game_paused;
 
     // Start is called before the first frame update
     void Start()
     {
-        heart1 = GameObject.FindGameObjectWithTag("Heart1");
-        heart2 = GameObject.FindGameObjectWithTag("Heart2");
-        heart3 = GameObject.FindGameObjectWithTag("Heart3");
-        heartsCanvas = GameObject.FindGameObjectWithTag("HeartsCanvas");
-        heartsCanvas.SetActive(true);
-        gameOverCanvas = GameObject.FindGameObjectWithTag("GameOverCanvas");
-        gameOverCanvas.SetActive(false);
+        // heartsCanvas.SetActive(true);
+        // gameOverCanvas = GameObject.FindGameObjectWithTag("GameOverCanvas");
+        // gameOverCanvas.SetActive(false);
         LoadComponents();
         GenerateLevel();
         sheep_controller.StartLevel();
@@ -51,48 +43,10 @@ public class GameController : MonoBehaviour
         level_spawner.GenerateLevel(GameData.level_pool_id);
     }
 
-    public void restartBtn()
-    {
-        GameData.isGameOver = false;
-        GameData.sheepLivesRemaining = GameData.sheepLivesInitial;
-        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
-    }
-
-    public void mainMenuBtn()
-    {
-        GameData.isGameOver = false;
-        GameData.sheepLivesRemaining = GameData.sheepLivesInitial;
-        StartCoroutine(LoadSceneAsync("Title"));
-    }
-
     // Call this whenever the sheep goes ragdoll.
     public void decrementLife()
     {
         GameData.sheepLivesRemaining--;
-        if (GameData.sheepLivesRemaining == 2)
-        {
-            heart3.SetActive(false);
-        }
-        else if (GameData.sheepLivesRemaining == 1)
-        {
-            heart2.SetActive(false);
-        }
-        else if (GameData.sheepLivesRemaining == 0)
-        {
-            heart1.SetActive(false);
-            heartsCanvas.SetActive(false);
-            gameOverCanvas.SetActive(true);
-            GameData.isGameOver = true;
-        }
-        
-    }
-    
-    IEnumerator LoadSceneAsync (string level)
-    {
-        op = SceneManager.LoadSceneAsync(level);
-        while (!op.isDone)
-        {
-            yield return null;
-        }
+        screen.updateHearts();
     }
 }
--- Assets/Scripts/Player/SheepController.cs
+++ Assets/Scripts/Player/SheepController.cs
@@ -33,8 +33,8 @@ public class SheepController : MonoBehaviour
 
     private void Start()
     {
-        LoadStates();
         LoadComponents();
+        LoadStates();
         SetDefaultState(idle_state);
 
         collided_obstacle = null;
